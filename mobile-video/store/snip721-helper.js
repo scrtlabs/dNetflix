4b344c18d05b92a4c2f19f3955dd5f5a59038ad8
TomL94 <langer.tom7@gmail.com>
2022-09-19 15:46:41 +0300

    Remove unnecessary function

--- mobile-video/store/snip721-helper.js
+++ mobile-video/store/snip721-helper.js
@@ -1,8 +1,7 @@
 export const getVideoCollectionPermit = async (secretJS, walletAddress, contractList, chainId) => {
-    if (!secretJS)
-        return null;
+    if (!secretJS) return null;
 
-    var permKey = `perm_${chainId}_${contractList.join("_")}_${walletAddress}`;
+    var permKey = `perm_${chainId}_${contractList.join('_')}_${walletAddress}`;
     var permit = null;
     try {
         // Try to load permit from local storage to avoid keplr confirmation in every visit
@@ -12,20 +11,14 @@ export const getVideoCollectionPermit = async (secretJS, walletAddress, contract
     if (!permit) {
         console.log('Loading new permit');
         try {
-            const result = await secretJS.utils.accessControl.permit.sign(
-                walletAddress,
-                chainId,
-                "scrtFlix-permit",
-                contractList,
-                ["balance", "owner"]                   
-            );
+            const result = await secretJS.utils.accessControl.permit.sign(walletAddress, chainId, 'scrtFlix-permit', contractList, ['balance', 'owner']);
             permit = result;
-            
+
             // Store the new permit in the browser local storage
             window.localStorage.setItem(permKey, JSON.stringify(permit));
             console.log('Permit loaded');
         } catch (err) {
-            console.log("Cannot load permit");
+            console.log('Cannot load permit');
             console.error(err);
         }
     } else {
@@ -35,8 +28,7 @@ export const getVideoCollectionPermit = async (secretJS, walletAddress, contract
 };
 
 export const getVideoTokens = async (secretJS, walletAddress, contractAddress, permit) => {
-    if (!secretJS)
-        return [];
+    if (!secretJS) return [];
 
     let tokens = [];
     try {
@@ -60,72 +52,3 @@ export const getVideoTokens = async (secretJS, walletAddress, contractAddress, p
         return [];
     }
 };
-
-export const mintToken = async (secretJS, walletAddress, contract) => {
-    const publicMetadata = {
-        extension: {
-            image: "ipfs://QmaK5Y969GeFqcBmu5BAPWgXfwkU9hpQCYJRJyQdYtCBjz",
-            description: "Encoded video example",
-            attributes: [
-                {
-                    trait_type: "Animal",
-                    value: "Bunny",
-                },
-            ],
-        },
-    };
-
-    const privateMetadata = {
-        extension: {
-            image: "ipfs://QmaK5Y969GeFqcBmu5BAPWgXfwkU9hpQCYJRJyQdYtCBjz",
-            description: "Encoded video example",
-            attributes: [
-                {
-                    trait_type: "Animal",
-                    value: "Big Bunny",
-                },
-            ],
-            media: [
-                {
-                    file_type: "video",
-                    extension: "m3u8",
-                    authentication: {
-                        key: "UainRqKrHz_62Gfx0Qv4Hg",
-                        user: null,
-                    },
-                    url: "ipfs://QmVbKFQRNx166RuqyyEb8XMwnw7GY57g7sXgcmxxKrL9ms/main.m3u8",
-                },
-            ],
-        },
-    };
-
-    const mintMsg = {
-        mint_nft: {
-            owner: walletAddress,
-            public_metadata: publicMetadata,
-            private_metadata: privateMetadata,
-        },
-    };
-
-    try {
-        let tx = await secretJS.tx.compute.executeContract(
-            {
-                sender: walletAddress,
-                contractAddress: contract.address,
-                codeHash: contract.codeHash,
-                msg: mintMsg,
-                sentFunds: [],
-            },
-            {
-                gasLimit: 60_000,
-            }
-        );
-        console.log(tx);
-        return true;
-    } catch (err) {
-        console.error(err);
-        return false;
-    }
-
-    
-}
